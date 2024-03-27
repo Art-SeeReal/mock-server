b2070869c6d3d719175c3799d822db0cfd0a7212
이소담 <daisydam@adictives.com>
2024-03-27 17:09:43 +0900

    feat: user api 추가

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 import express from "express";
 const router = express.Router();
 import users from "../data/users.js";
+import { maskLoginId, maskEmail } from "../utils/utils.js";
 
 router.get("/types", (req, res, next) => {
   try {
@@ -10,6 +11,65 @@ router.get("/types", (req, res, next) => {
         { code: "PLANNER", label: "기획자" },
       ],
     });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/userId", (req, res, next) => {
+  try {
+    const { name, email } = req.query;
+    const searchUser = users.find(
+      (user) => user.name === name && user.email === email
+    );
+    if (!searchUser) {
+      return res.status(404).json({ message: "가입된 정보가 없습니다." });
+    }
+    // 아이디는 마스킹 처리 후 내려줌.
+    res.json({ userId: maskLoginId(searchUser.userId) });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/exist", (req, res, next) => {
+  try {
+    const { name, email, userId } = req.query;
+
+    const searchUser = users.find(
+      (user) =>
+        user.name === name && user.email === email && user.userId === userId
+    );
+
+    if (!searchUser) {
+      return res.status(404).json({ message: "가입된 정보가 없습니다." });
+    }
+
+    // 이메일 인증코드 발송 로직
+    // ....
+    res.json({ message: "success", email: maskEmail(searchUser.email) });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/cert/email", (req, res, next) => {
+  try {
+    // 이메일 인증코드 확인 로직
+    if (req.body.certCode !== "xxxx") {
+      return res.status(400).json({ message: "인증코드가 올바르지 않습니다." });
+    }
+    res.json({ token: "0000" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/password", (req, res, next) => {
+  try {
+    // 토큰 확인 및 패스워드 변경 로직
+    // ....
+    res.json({ message: "success" });
   } catch (err) {
     next(err);
   }
---
+++ src/utils/utils.js
@@ -0,0 +1,26 @@
+export const maskLoginId = (loginId) => {
+  if (typeof loginId !== "string" || loginId.length === 0) {
+    return "";
+  }
+
+  // 맨 앞 2자리만 노출시키고 나머지를 마스킹 처리
+  const visibleChars = loginId.slice(0, 2);
+  const maskedPart = "*".repeat(loginId.length - 2);
+
+  return visibleChars + maskedPart;
+};
+
+export const maskEmail = (email) => {
+  if (typeof email !== "string" || email.length === 0) {
+    return "";
+  }
+
+  const parts = email.split("@");
+  const username = parts[0];
+  const domain = parts[1];
+
+  // 맨 앞글자만 노출시키고 나머지를 마스킹 처리
+  const maskedUsername = username.charAt(0) + "*".repeat(username.length - 1);
+
+  return maskedUsername + "@" + domain;
+};
